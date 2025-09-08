4c21f09cacd554475728dc0ad8d042ec735dadad
Guilherme Carvalho <99284514+guilhermevon@users.noreply.github.com>
2025-09-08 17:07:39 -0300

    Update exercicios.java ❤️

--- exercicios.java
+++ exercicios.java
@@ -10,7 +10,7 @@ public class Exercicios {
         System.out.print("Digite seu último nome: ");
         String ultimoNome = sc.nextLine();
         System.out.println(ultimoNome + ", " + primeiroNome + ".");
-        System.out.println("--------------------------------");
+        System.out.println("-");
 
         // 1.2 - Idade
         System.out.print("Digite seu ano de nascimento: ");
@@ -18,7 +18,7 @@ public class Exercicios {
         int anoAtual = java.time.Year.now().getValue();
         int idade = anoAtual - anoNascimento;
         System.out.println("Sua idade é: " + idade + " anos.");
-        System.out.println("--------------------------------");
+        System.out.println("-");
 
         // 1.3 - Soma de dois números
         System.out.print("Digite o primeiro número: ");
@@ -27,7 +27,7 @@ public class Exercicios {
         double num2 = sc.nextDouble();
         double soma = num1 + num2;
         System.out.println("A soma é: " + soma);
-        System.out.println("--------------------------------");
+        System.out.println("-");
 
         // 1.4 - Média de 5 notas
         double totalNotas5 = 0;
@@ -37,7 +37,7 @@ public class Exercicios {
         }
         double media5 = totalNotas5 / 5;
         System.out.println("A média das 5 notas é: " + media5);
-        System.out.println("--------------------------------");
+        System.out.println("-");
 
         // 1.5 - Média de 3 notas
         double totalNotas3 = 0;
@@ -47,7 +47,7 @@ public class Exercicios {
         }
         double media3 = totalNotas3 / 3;
         System.out.println("A média das 3 notas é: " + media3);
-        System.out.println("--------------------------------");
+        System.out.println("-");
 
         // 1.6 - Valor por hora trabalhada
         System.out.print("Digite seu salário total: ");
@@ -56,9 +56,10 @@ public class Exercicios {
         double horasPorDia = sc.nextDouble();
         double valorPorHora = salario / (22 * horasPorDia);
         System.out.println("Você recebe aproximadamente R$ " + valorPorHora + " por hora.");
-        System.out.println("--------------------------------");
+        System.out.println("-");
 
         sc.close();
     }
 }
+
 
